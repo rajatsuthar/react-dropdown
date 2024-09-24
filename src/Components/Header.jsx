fdfd4c69ad233cf87ab95de361e7292a9eb9a01e
rajat suthar <36098289+rajatsuthar@users.noreply.github.com>
2024-09-24 11:34:42 +0530

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="w-full bg-blue-600 flex justify-between px-7 py-2 text-white font-semibold">
+      <div className="text-2xl">React Dropdown</div>
+      <div className="text-xl">Skill Test </div>
+    </div>
+  );
+}
+
+export default Header;
